5da580106f0237a32e62b5db7e922044f040e822
Appoline Cogan <appolinecogan@gmail.com>
2022-10-12 11:13:23 +1100

    fixed connection and update user model

--- models/Users.js
+++ models/Users.js
@@ -1,7 +1,5 @@
 const { Schema, model } = require("mongoose");
 
-const thoughtSchema = require("./Thought");
-
 const userSchema = new Schema(
   {
     username: { type: String, required: true, unique: true, trim: true },
